0db266c8ae87823ed9d5db06ca8599abc6985583
Otto Jahnukainen <otto.jahnukainen@gmail.com>
2024-11-26 15:49:18 +0200

    feat: task 4.1 Add connection to database add dotenv add middleware and add way to test adding blog

--- osa_4/index.js
+++ osa_4/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const cors = require('cors')
-const mongoose = require('mongoose')
 
-const blogSchema = mongoose.Schema({
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+
+const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
@@ -12,31 +15,82 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = process.env.MONGODB_URI
+
 mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
+//middleware
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if (error.name === 'CastError'){
+    return response.status(400).send({ error: 'malformated id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const rndIDNumber = () => {
+  const id = Math.floor(Math.random() * 9000)
+  return String(id)
+}
+
+app.get('/api/blogs', (request, response, next) => {
   Blog
     .find({})
     .then(blogs => {
-      response.json(blogs)
+      if(blogs)
+        {response.json(blogs)}
+      else
+        {response.status(404).end()}
+      })
+    .catch(error => {
+      next(error)
     })
 })
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.post('/api/blogs', (request, response,next ) => {
+  const body = request.body
 
-  blog
+  if(!body.title || !body.url){
+    return response.status(404).json({
+      error: 'Content missing',
+      body: body
+    })
+  }  
+  const newBlog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    id: rndIDNumber(),
+  })
+
+  newBlog
     .save()
     .then(result => {
-      response.status(201).json(result)
+      if(result){
+        response.status(201).json(result)}
+      else{
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      next(error)
     })
 })
 
-const PORT = 3003
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
